fa436a4b9d96511dadccca53529934077b03c457
ryoki-0703 <tacchimi504@gmail.com>
2021-06-06 09:45:03 +0000

    tasksテーブル,nameカラムにNotNull制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_054951) do
+ActiveRecord::Schema.define(version: 2021_06_06_094200) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
